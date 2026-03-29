e19da7db6a8409feb1eb31931072396ba3cc3751
Shubham jakate <shubhamjakate0505@gmail.com>
2026-03-29 15:47:07 +0530

    fix base url

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,6 +1,6 @@
 // require("dotenv").config();
 // const BASE_URL=process.env.REACT_APP_BASE_URL;
-const BASE_URL="http://localhost:4000/api/v1";
+const BASE_URL=process.env.REACT_APP_BASE_URL;
 // console.log("BASE_URL ", process.env.REACT_APP_BASE_URL);
 // AUTH ENDPOINTS
 export const endpoints = {
